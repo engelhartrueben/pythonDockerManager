f745d4b98317eac917206491c8fd9145c77127e8
Rueben Engelhart <engelhartrueben@gmail.com>
2025-11-05 11:09:23 -0500

    comments

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,9 @@
+"""
+Works locally but unable to get working with GH actions.
+
+Unsure how to test this anyways at the moment. Holding off for some time.
+"""
+
 # from fastapi.testclient import TestClient
 # import roker.api.main as api
 # import pytest
